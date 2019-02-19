e3a6942628cb05485d54fe9a587e58bd5bcd22aa
paul-do <pauldo247@gmail.com>
2019-02-19 12:14:46 -0800

    Setup initial proxy server with Reservation component

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.port || 3050;
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(port, console.log(`proxy server is now listening on port ${port}`));
