99d18a6609eea2007e68a3157538a0d0938fedee
paul-do <pauldo247@gmail.com>
2019-02-19 21:45:08 -0800

    Serve components with css positioning

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const port = process.env.port || 3050;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './public')));
+// app.use(express.static(path.join(__dirname + './node_modules')));
 
 app.listen(port, console.log(`proxy server is now listening on port ${port}`));
